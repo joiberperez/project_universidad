a09fa9cb88e9066be56e15914f458e553eba0065
joiberperez <joiberperez20@gmail.com>
2024-09-07 11:46:33 -0400

    optimizacion de render templates

--- app/views/views.php
+++ app/views/views.php
@@ -1,83 +1,76 @@
 <?php
 
-class View extends ViewRender{
+class View extends ViewRender
+{
     public $template = '';
     public $model = null;
     public $object = null;
-    public function dispath(){
-        if($_SERVER['REQUEST_METHOD'] === 'GET'){
+    public function dispath()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
             $this->get();
         }
-        if($_SERVER["REQUEST_METHOD"] === 'POST'){
+        if ($_SERVER["REQUEST_METHOD"] === 'POST') {
             $this->post();
         }
     }
 
 
-   public function get(){
-        if(!$this->object){
+    public function get()
+    {
+        if (!$this->object) {
             $this->render($this->template);
-
-            
-            
-        }else{
+        } else {
             //echo (json_encode($this->model->getDetail($this->object)));
             //$this->model->disconnect();
-            
+
         }
         //print_r(json_encode($this->model->getALL()));
     }
-    public function post(){
-        
-            $dato = json_decode(file_get_contents("php://input"));
-            
-            $this->model->create($dato);
-            
+    public function post()
+    {
 
-      
+        $dato = json_decode(file_get_contents("php://input"));
+
+        $this->model->create($dato);
     }
-     public function __construct()
+    public function __construct()
     {
         $this->dispath();
     }
-
 }
 
-class ViewRender {
-    function render($template){
+class ViewRender
+{
+    function render($template)
+    {
+        $error = "";
         include APPS;
-        if(in_array("templates",scandir(ROOT))){
-            if(file_exists(ROOT . "/templates/". $template . ".php")){
-                 
-              include ROOT . "/templates/" . $template . ".php";
-            //}else{
-            }else{
-                $encontrado = 0;
-                foreach($apps as $app){
+        if (in_array("templates", scandir(ROOT))) {
+            if (file_exists(ROOT . "/templates/" . $template . ".php")) {
+
+                include ROOT . "/templates/" . $template . ".php";
+            } else {
+
+                foreach ($apps as $app) {
                     $ruta = ROOT . '/' . $app;
-                    if(in_array("templates",scandir($ruta))){
-                        if(file_exists($ruta . "/templates/". $template . ".php")){
-                          $encontrado = 1;   
-                          include $ruta . "/templates/" . $template . ".php";
-    
-                          break;
-                        //}else{
-                        }else{
-                            $error = true;
-                            
-                       }
+                    if (in_array("templates", scandir($ruta))) {
+                        if (file_exists($ruta . "/templates/" . $template . ".php")) {
+
+                            include $ruta . "/templates/" . $template . ".php";
+
+                            break;
+                        } else {
+                            $error = "No se ha encontrado la plantilla";
+                        }
                     }
                 }
-                $error = ($encontrado > 0) ?  "" : "La plantilla no existe";
-                if(!$error){echo $error;}
-                
-           }
-    
-        }else{
-            echo "la plantilla no existe";
-            
+            }
+        } else {
+            $error = "la carpeta templates principal no existe";
+        }
+        if ($error) {
+            echo $error;
         }
     }
 }
-
-?>
