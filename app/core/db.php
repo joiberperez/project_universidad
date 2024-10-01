db442ba7f55f62cc6fe7384e498aa85c5eae510d
Sathoru-Dev <joyner.7804@gmail.com>
2024-10-01 15:41:03 -0400

    Creacion de archivo de configuracion de base de datos

---
+++ app/core/db.php
@@ -0,0 +1,14 @@
+<?php
+    /*********************************************************************
+     * Revisión y depuración realizadas por "Sathoru-Dev"                *
+     * Revisado el dia: Domingo, 29 de Septiembre del 2024               *
+     * Por favor abstenerse de editar algo sin permiso del programador   *
+     * Copyright © Joiber (Uso educativo) - Proyecto universitario       *
+    *********************************************************************/
+
+    /** variables con datos de conexion a la base */
+    $driver = "mysql";
+    $host = "localhost";
+    $database = "chapulin";
+    $user = "sathoru";
+    $passwd = "TssJpo2001*";
